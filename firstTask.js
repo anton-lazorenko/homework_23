544521173d0ed28115779a42b052d41312408a6b
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-02-23 12:54:33 +0200

    first commit

---
+++ firstTask.js
@@ -0,0 +1,10 @@
+// let year = 1900;
+
+// while (year <= 2100) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//         console.log(year + "Высокосный");
+//     } else {
+//         console.log(year + "Не высокосный");
+//     } 
+//     year++;
+// }
