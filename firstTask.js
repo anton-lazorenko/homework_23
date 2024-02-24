b9b3496e1961fb29e604c8267c76f8edbf5d3c86
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-02-24 09:22:43 +0200

    uncomment the code

--- firstTask.js
+++ firstTask.js
@@ -1,10 +1,10 @@
-// let year = 1900;
+let year = 1900;
 
-// while (year <= 2100) {
-//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-//         console.log(year + "Высокосный");
-//     } else {
-//         console.log(year + "Не высокосный");
-//     } 
-//     year++;
-// }
+while (year <= 2100) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        console.log(year + "Высокосный");
+    } else {
+        console.log(year + "Не высокосный");
+    } 
+    year++;
+}
