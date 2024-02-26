a077ad425aebfadae4627c839d8e87b35725361e
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-02-26 11:21:12 +0200

    Fixed

--- secondTask.js
+++ secondTask.js
@@ -1,33 +1,38 @@
-let balance = 1000;
-let stavka;
+let balance = +prompt('Введите сумму до 20000');
 let win;
 let randomNumber;
 let color;
-let randomColor; 
+let randomColor;
 
-while (balance>0 && balance <20000) {  
-    let bet = +prompt(`Сделай ставку не более ${balance}`); 
-    if (!bet){
+while (balance > 0 && balance < 20000) {
+    let bet = +prompt(`Сделай ставку не более ${balance}`);
+    if (bet > balance) {
+        alert('Ставка превышает баланс')
         break
     }
-    let color = prompt('На какой цвет будем ставить? красный или черный?')
-    if (!color){
+    if (!bet) {
+        alert('Выберите корректное значение ставки')
         break
     }
-    if(color!= "красный" && color!= "черный"){
-        continue
+    let color = prompt('На какой цвет будем ставить? Red или Black?')
+    if (!color) {
+        alert('Введите корректное значение')
+        break
+    }
+    if (color != "Red" && color != "Black") {
+        alert('Выберите цвет')
+        break
     } else {
-        randomNumber = Math.floor(Math.random() * 2) + 1;
-        randomColor =  randomNumber == 1 ? "красный":"черный";
-            if (color == randomColor) {
-                win = bet*3;
-                balance += win;
-                alert(`Ты победил. Баланс увеличен! Баланс: ${balance}`)
-            } else {
-                balance -= bet;
-                bet*2;
-                console.log(bet)
-                alert(`Ты проиграл! Твой баланс сейчас - ${balance}`) 
-            }  
-        } 
+        isRed = Math.floor(Math.random() * 2) + 1; 
+        if (isRed) {
+            win = +bet * 3;
+            balance += win;
+            alert(`Ты победил. Баланс увеличен! Баланс: ${+balance}`)
+        } else {
+            balance -= bet;
+            +bet * 2;
+            console.log(bet)
+            alert(`Ты проиграл! Твой баланс сейчас - ${+balance}`)
+        }
     }
+}
